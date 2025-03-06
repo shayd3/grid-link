11066fa46a9455e4b59d20486afbd4372c14ae1f
shayd3 <ryan.ross215@gmail.com>
2025-03-06 18:42:54 -0500

    server init

---
+++ server/src/server.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+});
+
+server.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
